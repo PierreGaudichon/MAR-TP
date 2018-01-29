cb1c827d146cc1d8c691141c44fb1e035facd9c8
PierreGaudichon <pierre@gaudichon.com>
2018-01-29 11:38:27 +0100

    Add splittable bezier curve.

--- livraison/myJS/Bezier.js
+++ livraison/myJS/Bezier.js
@@ -18,12 +18,14 @@ function add(r, p, k) {
 // Curve3 :: Number -> Vector3
 // Number t in [0, 1]
 
+// Control3 :: Vector3[4]
 
-// cubic :: (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) -> Curve3
+
+// cubic :: Control3 -> Curve3
 // Calculate the cubic bezier curve from 4 points.
 // https://fr.wikipedia.org/wiki/Courbe_de_B%C3%A9zier#Courbes_de_B%C3%A9zier_cubiques
 //
-Bezier.cubic = function(p0, p1, p2, p3) {
+Bezier.cubic = function([p0, p1, p2, p3]) {
 	return function(t) {
 		var r = new THREE.Vector3(0, 0, 0);
 		var a = (1-t)*(1-t)*(1-t);
@@ -39,9 +41,9 @@ Bezier.cubic = function(p0, p1, p2, p3) {
 };
 
 
-// cubicdt :: (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) -> Curve3
+// cubicdt :: Control3 -> Curve3
 // Calculate the first derivative of the cubic bezier curve.
-Bezier.cubicdt = function(p0, p1, p2, p3) {
+Bezier.cubicdt = function([p0, p1, p2, p3]) {
 	return function(t) {
 		var r = new THREE.Vector3(0, 0, 0);
 		add(r, p0, -3*t*t); add(r, p1, 9*t*t); add(r, p2, -9*t*t); add(r, p3, 3*t*t);
@@ -52,15 +54,47 @@ Bezier.cubicdt = function(p0, p1, p2, p3) {
 };
 
 
-// cubicdt2 :: (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) -> Curve3
+// cubicdt2 :: Control3 -> Curve3
 // Calculate the second derivative of the bezier curve.
-Bezier.cubicdt2 = function(p0, p1, p2, p3) {
+Bezier.cubicdt2 = function([p0, p1, p2, p3]) {
 	return function(t) {
 		var r = new THREE.Vector3(0, 0, 0);
 		add(r, p0, -6*t); add(r, p1, 18*t); add(r, p2, -18*t); add(r, p3, 3*t);
 		add(r, p0, 6); add(r, p1, -12); add(r, p2, 6);
 	};
 };
+
+// length :: Control3 -> Number
+// Calculate the length of the bezier curve (cheap, not accurate).
+// https://stackoverflow.com/a/37862545/3765413
+Bezier.length = function([p0, p1, p2, p3]) {
+	function al(a, b) { a.clone().add(b).length(); }
+	var chord = p3.clone().sub(p0).length();
+	var net = al(p0, p1) + al(p1, p2) + al(p2, p3);
+	return (chord + net) / 2;
+}
+
+// subControls :: Number endpoint, Control3 -> Control3
+// Split the bezier curve from start to endpoint.
+// https://stackoverflow.com/a/8405756/3765413
+Bezier.subControls = function(endpoint, [p0, p1, p2, p3]) {
+	var p01 = p1.clone().sub(p0).multiplyScalat(endpoint).add(p0);
+	var p12 = p2.clone().sub(p1).multiplyScalat(endpoint).add(p1);
+	var p23 = p3.clone().sub(p2).multiplyScalat(endpoint).add(p2);
+	var p012 = p12.clone().sub(p01).multiplyScalat(endpoint).add(p01);
+	var p123 = p23.clone().sub(p12).multiplyScalat(endpoint).add(p12);
+	var p0123 = p123.clone().sub(p012).multiplyScalat(endpoint).add(p012);
+	return [p0, p01, p012, p0123];
+}
+
+// subControls :: Number endpoint, Control3 -> [Control3, Control3]
+// Split the bezier curve from start to endpoint. Returns both parts of the splitted curve.
+Bezier.split = function(endpoint, control) {
+	return [
+		Bezier.subControls(endpoint, control),
+		Bezier.subControls((1-endpoint), control.reverse()).reverse()
+	];
+}
 
 
 // addZ :: (Vector2, Number) -> Vector3
